b3660a2fab4233c0668590abeaa19e2f4d67e051
김형주 <khj14753@kggroup.co.kr>
2025-07-11 13:42:16 +0900

    chore: 주석 추가

--- kimhyeongju/day03/fetchData.js
+++ kimhyeongju/day03/fetchData.js
@@ -2,9 +2,11 @@ const API_URL = 'https://jsonplaceholder.typicode.com/posts/1';
 
 export const getFetchData = async () => {
     try{
+        // fetch로 데이터 습득
         let response = await fetch(API_URL);
         if (!response.ok) throw new Error();
     
+        // form 세팅
         let data = await response.json();
         document.getElementById("username").value = data.userId;
         document.getElementById("title").value = data.title;
