f1a3e31eb5cff1a8a4a2650b34172aa42a839405
suhsein <mini9075@kggroup.co.kr>
2025-07-11 16:38:52 +0900

    서세인 - Day 3 과제 제출
    - 콜론, 콤마 indentation 수정
    - 메세지 상수 모듈로 분리
    - DOM 변수 전역변수로 수정
    - fetch 로직 수정 및 예외 처리 추가

--- suhsein/day02/assignment/bbs-write.js
+++ suhsein/day02/assignment/bbs-write.js
@@ -1,73 +1,76 @@
+import { error_messages, messages, regex } from "./constant/Message.js";
+
 // 전역 DOM 변수
-    const $password        = document.getElementById("password");
-    const $passwordConfirm = document.getElementById("passwordConfirm");
-    const $message         = document.getElementById("message");
+const $password        = document.getElementById("password");
+const $passwordConfirm = document.getElementById("passwordConfirm");
+const $message         = document.getElementById("message");
+const $writer          = document.getElementById("writer");
+const $title           = document.getElementById("title");
+const $content         = document.getElementById("content");
+const $writeForm       = document.getElementById("writeForm");
 
 
-    // 비밀번호 keyup 이벤트
-    $password.addEventListener("keyup", equalsPassword);
-    // 비밀번호 확인 keyup 이벤트
-    $passwordConfirm.addEventListener("keyup", equalsPassword);
+// 비밀번호 keyup 이벤트
+$password.addEventListener("keyup", equalsPassword);
+// 비밀번호 확인 keyup 이벤트
+$passwordConfirm.addEventListener("keyup", equalsPassword);
+// 폼 제출 이벤트 (유효성 검증)
+$writeForm.addEventListener("submit", onFormSubmit);
 
 
-    // 폼 제출 이벤트 (유효성 검증)
-    document
-      .getElementById("writeForm")
-      .addEventListener("submit", onSubmitAction);
+// 비밀번호, 비밀번호 확인 동일 검증
+function equalsPassword(e) {
+  const password        = $password.value.trim();
+  const passwordConfirm = $passwordConfirm.value.trim();
+
+  $message.style.display = password === passwordConfirm ? "none" : "block";
+}
 
 
-    // 비밀번호, 비밀번호 확인 동일 검증
-    function equalsPassword(e) {
-      const password        = $password.value.trim();
-      const passwordConfirm = $passwordConfirm.value.trim();
+// 작성자, 제목, 내용, 비밀번호 유효성 검증
+function onFormSubmit(e) {
+  e.preventDefault();
 
-      $message.style.display = password === passwordConfirm ? "none" : "block";
-    }
+  const writer  = $writer.value.trim();
+  const title   = $title.value.trim();
+  const content = $content.value.trim();
 
+  // 작성자 유효성 검증
+  if (!writer) {
+    alert(error_messages.ERROR_EMPTY_WRITER);
+    return;
+  } else if (!regex.writerRegex.test(writer)) {
+    alert(error_messages.ERROR_INVALID_WRITER);
+    return;
+  }
 
-    // 작성자, 제목, 내용, 비밀번호 유효성 검증
-    function onSubmitAction(e) {
-        e.preventDefault();
+  // 제목 유효성 검증
+  if (!title) {
+    alert(error_messages.ERROR_EMPTY_TITLE);
+    return;
+  }
 
-        const writer  = document.getElementById("writer").value.trim();
-        const title   = document.getElementById("title").value.trim();
-        const content = document.getElementById("content").value.trim();
+  // 내용 유효성 검증
+  if (!content || content.length < 20) {
+    alert(error_messages.ERROR_INVALID_CONTENT);
+    return;
+  }
 
-        const writerRegex   = /^[ㄱ-ㅎ가-힣]+$/;
-        const passwordRegex = /^(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z0-9]{6,12}$/;
+  const password        = $password.value.trim();
+  const passwordConfirm = $passwordConfirm.value.trim();
 
-        if (!writer) {
-          alert("작성자는 빈 칸일 수 없습니다.");
-          return;
-        } else if (!writerRegex.test(writer)) {
-          alert("작성자 입력값은 한글만 허용됩니다.");
-          return;
-        }
+  // 비밀번호 유효성 검증
+  if (!password || !regex.passwordRegex.test(password)) {
+    alert(error_messages.ERROR_INVALID_PASSWORD);
+    return;
+  }
 
-        if (!title) {
-          alert("제목은 빈 칸일 수 없습니다.");
-          return;
-        }
+  // 비밀번호 불일치
+  if (password !== passwordConfirm) {
+    alert(error_messages.ERROR_NOT_EQUAL_PASSWORD);
+    return;
+  }
 
-        if (!content || content.length < 20) {
-          alert("내용은 최소 20자 이상으로 입력해주세요.");
-          return;
-        }
-
-        const password        = $password.value.trim();
-        const passwordConfirm = $passwordConfirm.value.trim();
-
-        if (!password || !passwordRegex.test(password)) {
-          alert(
-            "비밀번호는 6자 이상 12자 이하의 영문/숫자 조합으로 입력해주세요."
-          );
-          return;
-        }
-
-        if (password !== passwordConfirm) {
-          alert("비밀번호가 일치하지 않습니다.");
-          return;
-        }
-
-        alert("게시글 작성이 완료되었습니다.");
-    }
+  // 글쓰기 성공 메세지
+  alert(messages.WRITE_MEESAGE);
+}
