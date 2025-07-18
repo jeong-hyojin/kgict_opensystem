3c0501d5adb44a6203683bcf2e62635fbf313373
김형주 <khj14753@kggroup.co.kr>
2025-07-18 17:22:19 +0900

    chore: 불필요한 import 제거

--- kimhyeongju/intern-study/src/main/java/com/intern/study/board/controller/BoardController.java
+++ kimhyeongju/intern-study/src/main/java/com/intern/study/board/controller/BoardController.java
@@ -1,24 +1,12 @@
 package com.intern.study.board.controller;
 
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Map;
-import java.util.Set;
-import java.util.UUID;
-
 import com.intern.study.board.dto.BoardCreateRequest;
 import com.intern.study.board.dto.BoardUpdateRequest;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
-
-import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.*;
 
 @Slf4j
 @RestController
